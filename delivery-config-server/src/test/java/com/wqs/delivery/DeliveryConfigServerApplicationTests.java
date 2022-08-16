075c3720f47f9255b38efd369494cd44f20c7439
Wedson Quintanilha da Silva <wedson.silva@basis.com.br>
2022-08-16 14:22:56 -0300

    src add

---
+++ delivery-config-server/src/test/java/com/wqs/delivery/DeliveryConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wqs.delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

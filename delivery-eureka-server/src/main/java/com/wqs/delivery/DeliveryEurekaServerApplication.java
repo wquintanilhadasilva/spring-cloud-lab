3f4a2a458ca95f4e9bff569cff6a7b8fe4566d65
Wedson Quintanilha da Silva <wedson.silva@basis.com.br>
2022-08-16 14:47:28 -0300

    eureka src add

---
+++ delivery-eureka-server/src/main/java/com/wqs/delivery/DeliveryEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.wqs.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryEurekaServerApplication.class, args);
+	}
+
+}

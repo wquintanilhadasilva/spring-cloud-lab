0353214d7fdff9c8dddf5bc32a0c5cddb9b486d3
Wedson Quintanilha da Silva <wquintanilhadasilva@gmail.com>
2022-09-09 12:48:04 -0300

    add auth server

---
+++ delivery-auth-server/src/main/java/com/wqs/delivery/deliveryauthserver/DeliveryAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.wqs.delivery.deliveryauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryAuthServerApplication.class, args);
+	}
+
+}
---
+++ delivery-auth-server/src/test/java/com/wqs/delivery/deliveryauthserver/DeliveryAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wqs.delivery.deliveryauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

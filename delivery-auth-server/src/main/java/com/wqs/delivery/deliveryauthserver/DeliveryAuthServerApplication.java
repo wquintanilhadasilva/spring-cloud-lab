6b0ea200b3dcbc37125cf69b213eafa265cea23d
Wedson Quintanilha da Silva <wquintanilhadasilva@gmail.com>
2022-09-09 15:25:22 -0300

    fix

--- delivery-auth-server/src/main/java/com/wqs/delivery/deliveryauthserver/DeliveryAuthServerApplication.java
+++ delivery-auth-server/src/main/java/com/wqs/delivery/deliveryauthserver/DeliveryAuthServerApplication.java
@@ -2,8 +2,9 @@ package com.wqs.delivery.deliveryauthserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication  //(exclude = {DataSourceAutoConfiguration.class })
 public class DeliveryAuthServerApplication {
 
 	public static void main(String[] args) {
